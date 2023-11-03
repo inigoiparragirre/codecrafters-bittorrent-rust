a91b7275bad8e8c1974d785cb85342c7216fe6e1
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-11-03 17:46:06 +0100

    Parse encoded number

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,20 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
 
     // If encoded_value starts with a digit, it's a number
     if encoded_value.starts_with('i') && encoded_value.ends_with('e') {
-    //     // Example: "5:hello" -> "hello"
-    //     let colon_index = encoded_value.find(':').unwrap();
-         let number_string = &encoded_value[1..encoded_value.len() - 1];
-         let number = number_string.parse::<i64>().unwrap();
-         //let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-         return serde_json::Value::String(number.to_string());
+        //     // Example: "i52e" -> "52"
+        //     let colon_index = encoded_value.find(':').unwrap();
+        let number_string = &encoded_value[1..encoded_value.len() - 1];
+        let number = number_string.parse::<i64>().unwrap();
+        //let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        return serde_json::Value::String(number.to_string());
+    }
+    if encoded_value.chars().next().unwrap().is_digit(10) {
+        // Example: "5:hello" -> "hello"
+        let colon_index = encoded_value.find(':').unwrap();
+        let number_string = &encoded_value[..colon_index];
+        let number = number_string.parse::<i64>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        return serde_json::Value::String(string.to_string());
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
