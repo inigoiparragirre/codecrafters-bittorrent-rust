a44af812ced368cd976442bb4707b54d382cf29b
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-11-04 17:30:40 +0100

    Parse encoded number

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str, index: usize) -> (serde_json::Value, usize) {
 
     // If encoded_value starts with a i, and ends with an e and inside it's a number
     if encoded_value.starts_with('i') {
@@ -14,7 +14,7 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let end_index = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[1..end_index];
         let number = number_string.parse::<i64>().unwrap();
-        return serde_json::Value::Number(number.into());
+        return (serde_json::Value::Number(number.into()), number_string.len());
     }
     // If encoded value is a string, it starts with a number, followed by a colon, followed by the string
     else if encoded_value.chars().next().unwrap().is_digit(10) {
@@ -23,7 +23,7 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
+        return (serde_json::Value::String(string.to_string()), string.len());
     }
     // If encoded value is a list, it starts with an l, ends with an e, and contains encoded values
     else if encoded_value.starts_with('l') && encoded_value.ends_with('e') {
@@ -31,23 +31,24 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let mut list = Vec::new();
         // We need to remove the l and e from the encoded value
         // let inside_encoded_value = &encoded_value[1..encoded_value.len() - 1];
-        let mut current_index = 1;
-        while encoded_value.len() > 0 && current_index < encoded_value.len() - 2 {
+        let mut current_index = index + 1;
+        while current_index < encoded_value.len() {
             let current_value = &encoded_value[current_index..];
             // println!("current_value: {}", current_value);
-            let decoded_value = decode_bencoded_value(current_value);
+            let (decoded_value, _index) = decode_bencoded_value(current_value, current_index);
             // println!("decoded_value: {}", decoded_value.to_string());
-            match decoded_value {
-                serde_json::Value::Number(_) => current_index += decoded_value.to_string().len() + 2,
-                serde_json::Value::String(_) => current_index += decoded_value.to_string().len(),
-                serde_json::Value::Array(_) => current_index += decoded_value.to_string().len() + 1,
-                _ => panic!("Unhandled decoded value: {}", decoded_value)
-            }
+            // match decoded_value {
+            //     // Fix current index values for each case
+            //     serde_json::Value::Number(_) => current_index += decoded_value.to_string().len() + 2,
+            //     serde_json::Value::String(_) => current_index += decoded_value.to_string().len(),
+            //     serde_json::Value::Array(_) => current_index += decoded_value.to_string().len() + 1,
+            //     _ => panic!("Unhandled decoded value: {}", decoded_value)
+            // }
 
             list.push(decoded_value);
 
         }
-        return serde_json::Value::Array(list);
+        return (serde_json::Value::Array(list), 0);
     }
     else {
         panic!("Unhandled encoded value: {}", encoded_value)
@@ -64,7 +65,7 @@ fn main() {
         // println!("Logs from your program will appear here!");
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let (decoded_value, _) = decode_bencoded_value(encoded_value, 0);
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
