bcecd37d4e9f5b08cdc6bbcca1f2a00f30958589
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-11-04 09:36:54 +0100

    Decode a list of number and string

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,41 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number = number_string.parse::<i64>().unwrap();
         return serde_json::Value::Number(number.into());
     }
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    // If encoded value is a string, it starts with a number, followed by a colon, followed by the string
+    else if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
+    }
+    // If encoded value is a list, it starts with an l, ends with an e, and contains encoded values
+    else if encoded_value.starts_with('l') && encoded_value.ends_with('e') {
+        // Example: "l5:helloi52ee" -> ["hello", 52]
+        let mut list = Vec::new();
+        // We need to remove the l and e from the encoded value
+        let inside_encoded_value = &encoded_value[1..encoded_value.len() - 1];
+        // println!("inside_encoded_value: {}", inside_encoded_value);
+        let mut current_index = 0;
+        while current_index < inside_encoded_value.len() - 1 {
+            let current_value = &inside_encoded_value[current_index..];
+            // println!("current_value: {}", current_value);
+            let decoded_value = decode_bencoded_value(current_value);
+            // println!("decoded_value: {}", decoded_value.to_string());
+            match decoded_value {
+                serde_json::Value::Number(_) => current_index += decoded_value.to_string().len() + 1,
+                serde_json::Value::String(_) => current_index += decoded_value.to_string().len(),
+                // serde_json::Value::Array(_) => current_index += decoded_value.to_string().len() + 1,
+                _ => panic!("Unhandled decoded value: {}", decoded_value)
+            }
+
+            list.push(decoded_value);
+
+        }
+        return serde_json::Value::Array(list);
+    }
+    else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
@@ -36,7 +63,6 @@ fn main() {
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         // println!("Logs from your program will appear here!");
-
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
