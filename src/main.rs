97e906631455bdd37e2bf5021a0d25e6dc1a91f3
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-11-05 18:38:09 +0100

    Update logic

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str, index: usize) -> (serde_json::Value, usize) {
+fn decode_bencoded_value(encoded_value: &str, _index: usize) -> (serde_json::Value, usize) {
 
     // If encoded_value starts with a i, and ends with an e and inside it's a number
     if encoded_value.starts_with('i') {
@@ -14,16 +14,16 @@ fn decode_bencoded_value(encoded_value: &str, index: usize) -> (serde_json::Valu
         let end_index = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[1..end_index];
         let number = number_string.parse::<i64>().unwrap();
-        return (serde_json::Value::Number(number.into()), number_string.len());
+        return (serde_json::Value::Number(number.into()), number_string.len() + 2);
     }
     // If encoded value is a string, it starts with a number, followed by a colon, followed by the string
-    else if encoded_value.chars().next().unwrap().is_digit(10) {
+    else if encoded_value.chars().next().is_some_and(|c| c.is_digit(10)) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return (serde_json::Value::String(string.to_string()), string.len());
+        return (serde_json::Value::String(string.to_string()), string.len() + number_string.len() + 1);
     }
     // If encoded value is a list, it starts with an l, ends with an e, and contains encoded values
     else if encoded_value.starts_with('l') && encoded_value.ends_with('e') {
@@ -31,24 +31,17 @@ fn decode_bencoded_value(encoded_value: &str, index: usize) -> (serde_json::Valu
         let mut list = Vec::new();
         // We need to remove the l and e from the encoded value
         // let inside_encoded_value = &encoded_value[1..encoded_value.len() - 1];
-        let mut current_index = index + 1;
-        while current_index < encoded_value.len() {
-            let current_value = &encoded_value[current_index..];
+        let mut current_index = 1;
+        while current_index < encoded_value.len() - 1 {
+            let current_value = &encoded_value[current_index..encoded_value.len() - 1];
             // println!("current_value: {}", current_value);
-            let (decoded_value, _index) = decode_bencoded_value(current_value, current_index);
+            let (decoded_value, item_index) = decode_bencoded_value(current_value, current_index);
             // println!("decoded_value: {}", decoded_value.to_string());
-            // match decoded_value {
-            //     // Fix current index values for each case
-            //     serde_json::Value::Number(_) => current_index += decoded_value.to_string().len() + 2,
-            //     serde_json::Value::String(_) => current_index += decoded_value.to_string().len(),
-            //     serde_json::Value::Array(_) => current_index += decoded_value.to_string().len() + 1,
-            //     _ => panic!("Unhandled decoded value: {}", decoded_value)
-            // }
-
+            current_index += item_index;
             list.push(decoded_value);
 
         }
-        return (serde_json::Value::Array(list), 0);
+        return (serde_json::Value::Array(list), current_index + 1);
     }
     else {
         panic!("Unhandled encoded value: {}", encoded_value)
