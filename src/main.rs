59db78e8ace11ade9aa90d5561bbbccef4e72bec
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-11-09 12:36:32 +0100

    Clean warnings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-// mod decode;
-use serde_json;
 use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::error::Error;
 
 mod decode;
 
@@ -14,7 +15,7 @@ struct Arguments {
 
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
@@ -24,12 +25,25 @@ fn main() {
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let (decoded_value, _) = crate::decode::decode_bencoded_value(encoded_value, 0);
+        // This is necessary to print for the tests
         println!("{}", decoded_value.to_string());
     } else if command == "info" {
         println!("info: {}", args[2]);
+        // Attempt to open the file
+        let mut file = File::open(&args[2])?;
+
+        // Read the contents of the file
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+
+        // Print the contents of the file
+        println!("File contents:\n{}", content);
+
+
     }
 
     else {
         println!("unknown command: {}", args[1])
     }
+    Ok(())
 }
