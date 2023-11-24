c51aa879e949b8421e9144e6a55aac2c5157f4ad
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-11-24 21:43:01 +0100

    Clean

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        //println!("Logs from your program will appear here!");
         let encoded_value = &args[2];
         let (decoded_value, _) = decode::decode_bencoded_value(encoded_value, 0);
         // This is necessary to print for the tests
