c80c8585a3bbed8786e61af6429b04977a2fcb4b
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2023-12-01 23:06:39 +0100

    Pass test for handshake response id

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ async fn main() -> Result<()> {
         "peers" => {
             // Read the file
             let content: &[u8] = &std::fs::read(&args[2])?;
-            read_info(content, &mut info_hash, &mut torrent)?;
+            read_info(content, &mut info_hash, &mut torrent, true)?;
             match make_peer_request(&info_hash, &torrent, peer_id).await {
                 Ok(_) => {
                     Ok(())
@@ -69,18 +69,18 @@ async fn main() -> Result<()> {
             // Read the file
             let content: &[u8] = &std::fs::read(&args[2])?;
             let socket_addr = args[3].parse::<SocketAddr>().context("Error parsing socket address")?;
-            read_info(content, &mut info_hash, &mut torrent)?;
+            read_info(content, &mut info_hash, &mut torrent, false)?;
             if let Ok(mut stream) = TcpStream::connect(socket_addr) {
                 let handshake = Handshake::new(info_hash, *b"00112233445566778899");
                 let serialized_bytes = bincode::serialize(&handshake).expect("Serialization failed for handshake");
-                println!("Serialized: {:?}", serialized_bytes);
+                //println!("Serialized: {:?}", serialized_bytes);
                 stream.write_all(&serialized_bytes).expect("Error writing to stream");
 
                 // Read data from the stream
-                let mut buffer = [0; 1024];
+                let mut buffer = [0; 256];
                 stream.read(&mut buffer).expect("Error reading from stream");
-                let response = String::from_utf8_lossy(&buffer);
-                println!("Handshake: {:?}", response);
+                let handshake_response: Handshake = bincode::deserialize(&buffer).expect("Error deserializing handshake");
+                println!("Peer ID: {}", handshake_response.peer_id.iter().map(|b| format!("{:02x}", b)).collect::<String>());
                 Ok(())
                 //stream.
             }
@@ -94,7 +94,7 @@ async fn main() -> Result<()> {
         "info" => {
             // Read the file
             let content: &[u8] = &std::fs::read(&args[2])?;
-            read_info(content, &mut info_hash, &mut torrent)
+            read_info(content, &mut info_hash, &mut torrent, true)
         }
         _ => {
             println!("unknown command: {}", args[1]);
@@ -107,7 +107,7 @@ async fn main() -> Result<()> {
 // Ok(())
 // }
 
-fn read_info(content: &[u8], info_hash: &mut [u8; 20], torrent: &mut Torrent) -> Result<()> {
+fn read_info(content: &[u8], info_hash: &mut [u8; 20], torrent: &mut Torrent, print: bool) -> Result<()> {
     let mut parser = decode::Parser::new(&content);
     match parser.parse() {
         Ok(decoded_value) => {
@@ -116,38 +116,38 @@ fn read_info(content: &[u8], info_hash: &mut [u8; 20], torrent: &mut Torrent) ->
                     let url_string = format!("{}", url);
                     let output = url_string.trim_matches('"'); // Remove quotes
                     torrent.announce = output.to_string();
-                    println!("Tracker URL: {}", output);
+                    if print {println!("Tracker URL: {}", output);}
                 }
 
                 if let Some(info) = map.get("info".as_bytes()) {
                     if let BencodeValue::BDictionary(map) = info {
                         if let Some(length) = map.get("length".as_bytes()) {
-                            println!("Length: {}", length);
+                            if print {println!("Length: {}", length);}
                             torrent.info.length = length.to_string().parse::<i64>().unwrap();
                         }
 
                         if let Some(length) = map.get("piece length".as_bytes()) {
-                            println!("Piece Length: {}", length);
+                            if print {println!("Piece Length: {}", length);}
                             torrent.info.piece_length = length.to_string().parse::<i64>().unwrap();
                         }
 
                         let mut encoder = encode::Encoder::new();
                         encoder.encode(info)?;
                         let hash = encoder.encode_sha1(info_hash);
-                        println!("Info Hash: {}", hash);
+                        if print {println!("Info Hash: {}", hash);}
 
 
                         if let Some(pieces_string) = map.get("pieces".as_bytes()) {
                             // Get the bytes string and represent as hexadecimal
                             // Represent hexadecimal hash of each piece
                             if let BencodeValue::BString(pieces_string) = pieces_string {
-                                println!("Piece Hashes:");
+                                if print {println!("Piece Hashes:");}
                                 let mut remaining_hash_data = &pieces_string[..];
                                 while !remaining_hash_data.is_empty() {
                                     let (hash, rest) = remaining_hash_data.split_at(20);
                                     remaining_hash_data = rest;
                                     let hash_in_hex = hex::encode(hash);
-                                    println!("{}", hash_in_hex);
+                                    if print {println!("{}", hash_in_hex);}
                                 }
                             }
                         }
